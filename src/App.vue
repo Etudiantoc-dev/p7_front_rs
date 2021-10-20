905f44d419ad2748c2f0c40313125c138ef8e129
Eric <e.dambrin@gmail.com>
2021-10-20 16:25:29 +0200

    ok

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,83 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <img
+        class="logo_header"
+        src="../src/assets/icon-left-font.png"
+        alt="Logo de l'entreprise Groupomania"
+      />
+      <!-- <FormConnexion msg="Réseau social des employés" /> -->
+      <h1>Réseau social des employé(e)s</h1>
+
+      <ul>
+        <!-- <FormConnexion /> -->
+        <li><a href="inscription">Inscription</a></li>
+        <li><a href="connexion">Connexion</a></li>
+        <li><a href="forum">Forum</a></li>
+      </ul>
+    </header>
+    <section>
+      <aside class="élément">
+    
+      
+        
+        <FormConnexion
+          V-for= "item in connexion"
+          :checkForm="checkForm"
+          :validEmail="validEmail"
+          :email="item.email"
+          :password="item.password"
+        />
+        </aside>
+      
+    </section>
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FormConnexion from "./components/FormConnexion.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
-</script>
+    FormConnexion,
+  },
+  data() {
+    
+    return
+      
+        [{
+          errors: [],
+          email: null,
+          password: null,
+        }]
+  },
+    
+  
 
+  methods: {
+    
+    checkForm: function(e) {
+      this.errors = [];
+      if (!this.email) {
+        this.errors.push("Email required.");
+      } else if (this.validEmail(this.email)) {
+        this.errors.push("Valid email required.");
+      }
+      if (!this.errors.length) {
+        return true;
+      }
+      e.preventDefault();
+    },
+
+    validEmail: function(email) {
+      var expReg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return expReg.test(email);
+    },
+  },
+};
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/components/FormConnexion.vue
@@ -0,0 +1,131 @@
+<script>
+export default {
+  name: "FormConnexion",
+  props: ["checkForm","validEmail","email","password"]
+};
+</script>
+<template>
+  <div class="form_group">
+    
+    <form id="app" 
+    @submit= "checkForm"
+    action="http://localhost:8080/ "
+    method="post"
+    novalidate="true"
+    >
+    <p v-if="errors.length">
+    <b>Please correct the following error(s):</b>
+    <ul>
+      <li> v-for="error in errors">{{ error }}</li>
+    </ul>
+  </p>
+      <div class="form_group">
+        <label for="email">Email</label>
+        <input
+          type="email"
+          class="form-control"
+          id="email"
+          name="email"
+          placeholder="Adresse Email"
+        />
+      </div>
+      <div class="form_group">
+        <label for="password">Mot de passe</label>
+        <input
+          type="text"
+          class="form-control"
+          id="password"
+          name="password"
+          placeholder="Mot de passe"
+        />
+      </div>
+      <div class="form_group">
+        <input
+          type="submit"
+          value="Connexion"
+          class="bouton_connexion"
+          id="bouton"
+        />
+        <p id="erreur" style="color: orangered; width: max-content"></p>
+      </div>
+    </form>
+  </div>
+</template>
+<style lang="css">
+body {
+  width: 100%;
+  margin: auto;
+}
+/* HEADER */
+header {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+}
+
+.logo_header {
+  width: 200px;
+  margin-top: -40px;
+}
+
+ul,
+a {
+  margin-right: 30px;
+  text-decoration: none;
+  list-style-type: none;
+  color: #fd2d01;
+}
+ul,
+li {
+  padding-top: 10px;
+}
+
+/* SECTION formulaire */
+.form_group {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 50%;
+  margin: auto;
+  height: 50px;
+}
+.form_group,
+label {
+  display: flex;
+  justify-content: center;
+}
+.bouton_connexion {
+  color: #fd2d01;
+  margin-top: 15px;
+}
+/* Responsive */
+
+@media (max-width: 768px) {
+  header {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: column;
+    width: 100%;
+    margin: auto;
+  }
+  .logo_header {
+    margin: auto;
+    width: 20%;
+  }
+  h1 {
+    font-size: 20px;
+    margin: auto;
+  }
+  ul,
+  a {
+    display: flex;
+    justify-content: center;
+    color: #fd2d01;
+    margin-top: 10px;
+    padding-left: 20px;
+  }
+  section {
+    margin-top: 20px;
+  }
+}
+</style>
