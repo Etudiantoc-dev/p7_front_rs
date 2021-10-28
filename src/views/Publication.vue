1e1572fe6682a05c22b4b16672d8eb77e6a37eb7
Eric <e.dambrin@gmail.com>
2021-10-28 18:50:45 +0200

    Début HTML Publication

--- src/views/Publication.vue
+++ src/views/Publication.vue
@@ -1,24 +1,56 @@
 <script>
 import Footer from "../components/Footer.vue";
 import Header from "../components/Header.vue";
-import {mapState} from "vuex"
+import { mapState } from "vuex";
 export default {
   name: "Publication",
   components: {
     Footer,
     Header,
   },
-   computed:{
+  computed: {
     ...mapState({
-      logo: "logo"
-    })
-}}
+      logo: "logo",
+    }),
+  },
+};
 </script>
-<template id="app" class="app">
-  <section>
-    <aside>
-      <Header v-for="item in logo" :image="item.image" :key="item.name" />
-      <Footer />
-    </aside>
-  </section>
+<template>
+  <div id="app" class="app">
+    <Header v-for="item in logo" :image="item.image" :key="item.name" />
+    <section>
+      <aside id="form_publication">
+        <div class="container_commentaires"></div>
+        <div class="container_img"></div>
+      </aside>
+      <aside></aside>
+    </section>
+    <Footer />
+  </div>
 </template>
+<style lang="css">
+.commentaires {
+  background-color: red;
+}
+.container_commentaires {
+  background: black;
+  width: 25%;
+  height: 200px;
+  object-fit: cover;
+  padding-top: 10px;
+}
+
+.container_img {
+  width: 25%;
+  height: 400px;
+  object-fit: cover;
+  padding-top: 10px;
+  background: red;
+}
+#form_publication{
+    margin: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+</style>
